56fe5859465f38b750bfff46d51eb5a7c79e9f6f
Duc dep zai <trinhduc99nd@gmail.com>
2020-08-18 15:24:11 +0700

    upload product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,10 +9,10 @@ $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'name_author' => $faker->name,
-        'category_id' => $faker->numberBetween(7,32),
+        'category_id' => $faker->numberBetween(1,6),
         'image_path' => $faker->imageUrl($width = 640, $height = 480),
         'image_name' => $faker->imageUrl($width = 640, $height = 480),
-        'user_id' => 23,
+        'user_id' => 1,
         'content' => $faker->text($maxNbChars = 50),
         'created_at' => new DateTime(),
         'updated_at' => new DateTime(),
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([
-//             ProductSeeder::class,
-             CategorySeeder::class,
+             ProductSeeder::class,
+//             CategorySeeder::class,
          ]);
     }
 }
