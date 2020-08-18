64481fdc2b25973739324d1cf0e37553a4a3adc9
Duc dep zai <trinhduc99nd@gmail.com>
2020-08-18 15:55:17 +0700

    update code

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call([
              ProductSeeder::class,
-//             CategorySeeder::class,
+             CategorySeeder::class,
          ]);
     }
 }
